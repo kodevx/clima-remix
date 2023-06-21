3af943f63e532e7e0610be94e733153facea11fb
Rohith <rohithnair2910@gmail.com>
2023-06-21 18:14:13 +0530

    Added Remove Location Feature Logic in Location List

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -7,7 +7,13 @@ import SearchBar from '../components/SearchBar';
 import WeatherSummary from '../components/WeatherSummary';
 
 import { formatWeatherData } from '../utils/formatWeatherData';
-import { getCitiesWeatherList, saveCityWeather } from '../models/weather.server';
+import { 
+    getCitiesWeatherList, 
+    saveCityWeather, 
+    removeWeatherLocation 
+} from '../models/weather.server';
+
+const REMOVE_LOCATION = 'remove-location';
 
 export const meta = () => {
     return [
@@ -30,36 +36,51 @@ export const loader = async ({ params, request }) => {
 export const action = async ({ request, params }) => {
 
     const formData = await request.formData();
-    const city = formData.get("city");
+    const city = formData.get('city');
+    const requestType = formData.get('request-type');
 
     let errorStatus = null;
     
     console.log("City: ",city);
 
-    try {
-        if(!city) {
-            throw 'CITY_ERROR';
+    if(requestType === REMOVE_LOCATION) {   // Remove Location from Weather List
+        console.log("remove location");
+        try {
+            console.log(`Remove City ${city}`);
+            const weatherData = await removeWeatherLocation(city);
+
+            return redirect(`/`);
+        } catch(error) { 
+            console.log("Error: ",error);
+
+            return null;
         }
-        
-        const res = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`
-        );
-        console.log("Weather Data: ",res.data);
+    } else {                            // Add Location to Weather List
+        try {
+            if(!city) {
+                throw 'CITY_ERROR';
+            }
+            
+            const res = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`
+            );
+            console.log("Weather Data: ",res.data);
 
-        const formattedData = formatWeatherData(res.data);
-        await saveCityWeather(formattedData);
+            const formattedData = formatWeatherData(res.data);
+            await saveCityWeather(formattedData);
 
-        errorStatus = null
-        
-    } catch (err) {
-        console.error("Weather API Error: ",err);
-        errorStatus = 'NOT_FOUND' 
-    }
+            errorStatus = null
+            
+        } catch (err) {
+            console.error("Weather API Error: ",err);
+            errorStatus = 'NOT_FOUND' 
+        }
 
-    if(city) {
-        return redirect(`/weather/${city}`);
-    } else {
-        return redirect(`/`);
+        if(city) {
+            return redirect(`/weather/${city}`);
+        } else {
+            return redirect(`/`);
+        }
     }
 }
 
@@ -69,36 +90,28 @@ const Index = () => {
 
     return (
         <div className="border-4 border-gray-950 p-10 m-10">
-            <Form
-                method="post"
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: 8,
-                    width: "100%",
-                }}
-            >
-                <div>
+            <div>
+                <Form method="post">
                     <div className="flex justify-center text-2xl font-gothamBold ml-1 mb-5 tracking-widest">
                         LOCATIONS
                     </div>
                     <SearchBar />
-                    <div>
-                        {cities && cities.length > 0 ? cities.map(data => (
-                            <WeatherSummary 
-                                key={data.id}
-                                data={data} 
-                            />
-                        )) : (
-                            <div className='flex justify-center'>
-                                <span className='text-lg text-white px-3 bg-black font-gothamMedium tracking-wide'>
-                                    NO CITIES ARE ADDED.
-                                </span>
-                            </div>
-                        )}
-                    </div>
+                </Form>
+                <div>
+                    {cities && cities.length > 0 ? cities.map(data => (
+                        <WeatherSummary 
+                            key={data.id}
+                            data={data} 
+                        />
+                    )) : (
+                        <div className='flex justify-center'>
+                            <span className='text-lg text-white px-3 bg-black font-gothamMedium tracking-wide'>
+                                NO CITIES ARE ADDED.
+                            </span>
+                        </div>
+                    )}
                 </div>
-            </Form>
+            </div>
         </div>
     )
 }
