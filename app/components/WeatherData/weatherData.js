241caa01582aae6144701a20bdc545acb8986c84
Rohith <rohithnair2910@gmail.com>
2023-07-27 09:59:27 +0530

    Added Styles of Dark Mode for Weather Info Page

--- app/components/WeatherData/weatherData.js
+++ app/components/WeatherData/weatherData.js
@@ -34,76 +34,76 @@ const WeatherData = props => {
             <div className='p-10'>
                 <div className='flex flex-col items-center my-6'>
                     <img src={weatherIcon} className='h-14 w-14 mb-4' alt={'weather-icon'} /> 
-                    <div className='font-gothamMedium text-2xl mt-4 tracking-wide'>{data.desc.toUpperCase()}</div>
-                    <div className='font-gothamMedium text-2xl mt-2'>{data.temp}°C</div>
+                    <div className='font-gothamMedium text-2xl mt-4 tracking-wide dark:text-orange-300'>{data.desc.toUpperCase()}</div>
+                    <div className='font-gothamMedium text-2xl mt-2 dark:text-orange-300'>{data.temp}°C</div>
                 </div>
-                <div className='flex flex-col group p-11 mb-5 shadow-xl transition duration-300 ease-in-out hover:translate-x-1 hover:scale-105'>
+                <div className='flex flex-col group p-11 mb-5 shadow-xl transition duration-300 ease-in-out hover:translate-x-1 hover:scale-105 dark:bg-neutral-800'>
                     <div className='flex'>
-                        <div className='text-lg mb-3 px-3 font-gothamMedium tracking-wider hover:transition duration-300 ease-in-out group-hover:bg-orange-300'>CURRENT WEATHER</div>
+                        <div className='text-lg mb-3 px-3 font-gothamMedium tracking-wider hover:transition duration-300 ease-in-out group-hover:bg-orange-300 dark:text-orange-300 dark:group-hover:text-black'>CURRENT WEATHER</div>
                     </div>
                     <div className='flex flex-row justify-around'>
                         <div className='flex flex-col items-center'>
-                            <span className='text-lg font-gothamLight'>Temperature</span>
-                            <span className='text-lg font-gothamMedium'>{data.temp}°C</span>
+                            <span className='text-lg font-gothamLight dark:text-stone-400'>Temperature</span>
+                            <span className='text-lg font-gothamMedium dark:text-stone-300'>{data.temp}°C</span>
                         </div>
                         <div className='flex flex-col items-center'>
-                            <span className='text-lg font-gothamLight'>Description</span>
-                            <span className='text-lg font-gothamMedium'>{data.desc}</span>
+                            <span className='text-lg font-gothamLight dark:text-stone-400'>Description</span>
+                            <span className='text-lg font-gothamMedium dark:text-stone-300'>{data.desc}</span>
                         </div>
                         <div className='flex flex-col items-center'>
-                            <span className='text-lg font-gothamLight'>Feels like</span>
-                            <span className='text-lg font-gothamMedium'>{data.feelsLike}°C</span>
+                            <span className='text-lg font-gothamLight dark:text-stone-400'>Feels like</span>
+                            <span className='text-lg font-gothamMedium dark:text-stone-300'>{data.feelsLike}°C</span>
                         </div>
                     </div>
                 </div>
-                <div className='flex flex-col mb-5 group p-11 shadow-xl transition duration-300 ease-in-out hover:translate-x-1 hover:scale-105'>
+                <div className='flex flex-col mb-5 group p-11 shadow-xl transition duration-300 ease-in-out hover:translate-x-1 hover:scale-105 dark:bg-neutral-800'>
                     <div className='flex'>
-                        <div className='text-lg font-gothamMedium tracking-wider mb-2 px-2 hover:transition duration-300 ease-in-out group-hover:bg-red-300'>TEMPERATURE EXTREMES</div>
+                        <div className='text-lg font-gothamMedium tracking-wider mb-2 px-2 hover:transition duration-300 ease-in-out group-hover:bg-red-300 dark:text-red-300 dark:group-hover:text-black'>TEMPERATURE EXTREMES</div>
                     </div>
                     <div className='flex justify-around'>
                         <div className='flex flex-col items-center'>
-                            <span className='text-lg font-gothamLight'>Min Temp</span>
-                            <span className='text-lg font-gothamMedium'>{data.tempMin}°C</span>
+                            <span className='text-lg font-gothamLight dark:text-stone-400'>Min Temp</span>
+                            <span className='text-lg font-gothamMedium dark:text-stone-300'>{data.tempMin}°C</span>
                         </div>
                         <div className='flex flex-col items-center'>
-                            <span className='text-lg font-gothamLight'>Max Temp</span>
-                            <span className='text-lg font-gothamMedium'>{data.tempMax}°C</span>
+                            <span className='text-lg font-gothamLight dark:text-stone-400'>Max Temp</span>
+                            <span className='text-lg font-gothamMedium dark:text-stone-300'>{data.tempMax}°C</span>
                         </div>
                     </div>
                 </div>
 
-                <div className='flex flex-col mb-5 group p-11 shadow-xl transition duration-300 ease-in-out group-hover:translate-x-1 hover:scale-105'>
+                <div className='flex flex-col mb-5 group p-11 shadow-xl transition duration-300 ease-in-out group-hover:translate-x-1 hover:scale-105 dark:bg-neutral-800'>
                     <div className='flex'>
-                        <div className='text-lg font-gothamMedium tracking-wider mb-2 px-2 hover:transition duration-300 ease-in-out group-hover:bg-teal-300'>
+                        <div className='text-lg font-gothamMedium tracking-wider mb-2 px-2 hover:transition duration-300 ease-in-out group-hover:bg-teal-300 dark:text-teal-300 dark:group-hover:text-black'>
                             PRESSURE & HUMIDITY
                         </div>
                     </div>
                     <div className='flex justify-around'>
                         <div className='flex flex-col items-center'>
-                            <span className='text-lg font-gothamLight'>Pressure</span>
-                            <span className='text-lg font-gothamMedium'>{data.pressure}</span>
+                            <span className='text-lg font-gothamLight dark:text-stone-400'>Pressure</span>
+                            <span className='text-lg font-gothamMedium dark:text-stone-300'>{data.pressure}</span>
                         </div>
                         <div className='flex flex-col items-center'>
-                            <span className='text-lg font-gothamLight'>Humidity</span>
-                            <span className='text-lg font-gothamMedium'>{data.humidity}</span>
+                            <span className='text-lg font-gothamLight dark:text-stone-400'>Humidity</span>
+                            <span className='text-lg font-gothamMedium dark:text-stone-300 '>{data.humidity}</span>
                         </div>
                     </div>
                 </div>
 
-                <div className='flex flex-col mb-5 group p-11 shadow-xl transition duration-300 ease-in-out hover:translate-x-1 hover:scale-105'>
+                <div className='flex flex-col mb-5 group p-11 shadow-xl transition duration-300 ease-in-out hover:translate-x-1 hover:scale-105 dark:bg-neutral-800'>
                     <div className='flex'>
-                        <div className='flex flex-col font-gothamMedium tracking-wider text-lg mb-2 px-2 hover:transition duration-300 ease-in-out group-hover:bg-gray-200'>
+                        <div className='flex flex-col font-gothamMedium tracking-wider text-lg mb-2 px-2 hover:transition duration-300 ease-in-out group-hover:bg-gray-200 dark:text-gray-200 dark:group-hover:text-black'>
                             WIND CONDITIONS
                         </div>
                     </div>
                     <div className='flex justify-around'>
                         <div className='flex flex-col items-center'>
-                            <span className='text-lg font-gothamLight'>Speed</span>
-                            <span className='text-lg font-gothamMedium'>{data.speed}</span>
+                            <span className='text-lg font-gothamLight dark:text-stone-400'>Speed</span>
+                            <span className='text-lg font-gothamMedium dark:text-stone-300'>{data.speed}</span>
                         </div>
                         <div className='flex flex-col items-center'>
-                            <span className='text-lg font-gothamLight'>Degree</span>
-                            <span className='text-lg font-gothamMedium'>{data.deg}</span>
+                            <span className='text-lg font-gothamLight dark:text-stone-400'>Degree</span>
+                            <span className='text-lg font-gothamMedium dark:text-stone-300'>{data.deg}</span>
                         </div>
                     </div>
                 </div>
@@ -112,18 +112,18 @@ const WeatherData = props => {
                 <div className='flex justify-end items-center'>
                     <div>
                         <input name='request-type' value='update-weather-info' readOnly hidden />
-                        <button type='submit' className='shadow-xl rounded-3xl mx-2 hover:bg-emerald-400 p-2 transition duration-300 ease-in-out hover:scale-110'>
+                        <button type='submit' className='shadow-xl rounded-3xl mx-2 hover:bg-emerald-400 p-2 transition duration-300 ease-in-out hover:scale-110 dark:bg-stone-300 dark:hover:bg-emerald-400'>
                             <img src={UpdateIcon} className='h-6 w-6' alt={'update-icon'} /> 
                         </button>
                     </div>
                     <div className='group font-gothamMedium text-sm'>
-                        <div className='flex justify-end tracking-wide'>
+                        <div className='flex justify-end tracking-wide dark:text-white'>
                             Last Updated at 
                             <span className='ml-1 group-hover:text-orange-600'>
                                 {localDateAndTime}
                             </span>
                         </div>
-                        <div className='text-xs text-gray-400 font-loveloLight italic tracking-wider'>*Time Zone based in INDIA</div>
+                        <div className='text-xs text-gray-400 font-loveloLight italic tracking-wider dark:text-gray-500'>*Time Zone based in INDIA</div>
                     </div>
                 </div>
             </Form>
--- app/routes/weather_.$cityName.jsx
+++ app/routes/weather_.$cityName.jsx
@@ -78,7 +78,7 @@ const CityWeather = (props) => {
             <div>
                 <div className="flex justify-between">
                     <Link className='flex justify-center items-center text-blue-500 h-14 w-32 text-lg hover:text-white hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:duration-300 rounded-full' to={`/`}>
-                        <img src={LeftArrow} className='h-4 w-4 mr-2 fill-white' alt={'weather-icon'} /> 
+                        <img src={LeftArrow} className='h-4 w-4 mr-2 fill-white' alt={'left-arrow'} /> 
                         <div className='font-gothamMedium tracking-wider'>BACK</div>    
                     </Link>
                     <Form method='post'>
@@ -92,7 +92,7 @@ const CityWeather = (props) => {
                             // offset={[-20, 5]} // [skidding, distance]
                         >
                             <button type='submit' className='group h-12 w-12 flex justify-center items-center transition duration-300 ease-in-out hover:scale-95 hover:text-white hover:bg-gradient-to-r from-orange-400 to-red-500 rounded-3xl'>
-                                <svg className='h-7 w-7 fill-black group-hover:fill-white' xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px">    
+                                <svg className='h-7 w-7 fill-black group-hover:fill-white dark:fill-white dark:group-hover:fill-black' xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px">    
                                     <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"/>
                                 </svg>
                             </button>
